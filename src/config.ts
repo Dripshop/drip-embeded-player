74f6ed4cedcc3edc7240f35cf231b6b88ca620cd
Ashwin <devcodeash@gmail.com>
2025-07-08 12:04:46 +0530

    Switch between user accounts

---
+++ src/config.ts
@@ -0,0 +1,14 @@
+export const UserAccounts = [
+  {
+    id: 1,
+    name: "User A",
+    token:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyNzY4MSwibG9naW5fa2V5IjpudWxsLCJ0b2tlbl9pZCI6IjByU0lzY2hOM19NU3J1ZFE1bXdPZCIsImlhdCI6MTc1MTg5NTc1NywiZXhwIjoxNzgzNDMxNzU3fQ.Fjj9q1euWhOFVT7m-QwxRDlXZuI2K2Dvoy35Nuszclg",
+  },
+  {
+    id: 2,
+    name: "User B",
+    token:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyODU1MiwibG9naW5fa2V5IjpudWxsLCJ0b2tlbl9pZCI6IjlhRXVNYk5ncVBQRnNvS3ZodjQ4NSIsImlhdCI6MTc1MTg5NTY4MCwiZXhwIjoxNzgzNDMxNjgwfQ.sYTPIErAuB91mdBBsa0OjgBsOnp159tDjTz_Sh0QdHM",
+  },
+];
